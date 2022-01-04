d61a1312c2f87275c1303001b7441858acc253a1
Adrianapt <56317116+Adrianapt@users.noreply.github.com>
2022-01-04 14:23:35 -0600

    Add files via upload

---
+++ diagrama.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.upv.fpoo.diagnostico;
+
+/**
+ *
+ * @author palmero
+ */
+public class diagrama {
+    
+  public static void main(String[] args) {
+      
+int numPlayers= 1;
+boolean status = false;
+boolean winner = false;
+int numPlayer = 0;
+if (numPlayer > 1){
+//System.out.println ('status');
+} else{
+    /*System.out.println ('salir');*/
+}
+
+ 
+}
+}
---
+++ juego.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.upv.fpoo.diagnostico;
+
+import java.util.Random;
+import java.util.Scanner;
+
+
+/**
+ *
+ * @author palmero
+ */
+public class juego {
+    
+    public static void main(String[] args) {
+
+int numusuario ;
+Random num = new Random(); 
+
+ 
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese un numero");
+        numusuario=scanner.nextInt();
+
+    if (numusuario = random) {  
+      System.out.println("correcto"); 
+     } else{
+         System.out.println("incorrecto"); 
+    }
+    
+    }
+  }
+
+
